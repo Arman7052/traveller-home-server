481241d5a86cf5aa5dcd7f8ed91e36cadf154a92
Arman7052 <99arman7052@gmail.com>
2023-10-06 00:06:47 +0600

    Basic crud oparetion done

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 
 
-// ------------------------------------------------------------//
+// ---------------MongoDB DATABASE-----------------------------------//
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -98,7 +98,7 @@ async function run() {
         });
 
 
-        // delete room
+        // Delete room
         app.delete('/rooms/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
